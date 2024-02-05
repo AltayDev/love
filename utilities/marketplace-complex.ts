3491309cc96b4f3e3592518c7019ac114042abcc
Oğuzhan <145108492+AltayDev@users.noreply.github.com>
2024-02-05 21:52:36 +0300

    Update marketplace-complex.ts
    - Change timestamp string to u64
    - remove buy history

--- utilities/marketplace-complex.ts
+++ utilities/marketplace-complex.ts
@@ -88,8 +88,8 @@ export class SellOffer implements Serializable {
     public tokenId: string = '',
     public price: u64 = 0,
     public creatorAddress: string = '',
-    public expirationTime: string = '',
-    public createdTime: string = '',
+    public expirationTime: u64 = 0,
+    public createdTime: u64 = 0,
   ) {}
 
   serialize(): StaticArray<u8> {
@@ -99,8 +99,8 @@ export class SellOffer implements Serializable {
     args.add<string>(this.tokenId);
     args.add<u64>(this.price);
     args.add<string>(this.creatorAddress);
-    args.add<string>(this.expirationTime);
-    args.add<string>(this.createdTime);
+    args.add<u64>(this.expirationTime);
+    args.add<u64>(this.createdTime);
     return args.serialize();
   }
 
@@ -123,66 +123,13 @@ export class SellOffer implements Serializable {
     if (creatorAddressResult.isErr()) return new Result(0);
     this.creatorAddress = creatorAddressResult.unwrap();
 
-    const expirationTimeResult = args.nextString();
+    const expirationTimeResult = args.nextU64();
     if (expirationTimeResult.isErr()) return new Result(0);
     this.expirationTime = expirationTimeResult.unwrap();
 
-    const createdTimeResult = args.nextString();
+    const createdTimeResult = args.nextU64();
     if (createdTimeResult.isErr()) return new Result(0);
     this.createdTime = createdTimeResult.unwrap();
-
-    return new Result(args.offset);
-  }
-}
-
-export class BuyTokenOperation implements Serializable {
-  constructor(
-    public collectionAddress: string = '',
-    public tokenId: string = '',
-    public price: u64 = 0,
-    public buyerAddress: string = '',
-    public sellerAddress: string = '',
-    public timestamp: string = '',
-  ) {}
-
-  serialize(): StaticArray<u8> {
-    const args = new Args();
-
-    args.add<string>(this.collectionAddress);
-    args.add<string>(this.tokenId);
-    args.add<u64>(this.price);
-    args.add<string>(this.buyerAddress);
-    args.add<string>(this.sellerAddress);
-    args.add<string>(this.timestamp);
-    return args.serialize();
-  }
-
-  deserialize(data: StaticArray<u8>, offset: i32): Result<i32> {
-    const args = new Args(data, offset);
-
-    const collectionAddressResult = args.nextString();
-    if (collectionAddressResult.isErr()) return new Result(0);
-    this.collectionAddress = collectionAddressResult.unwrap();
-
-    const tokenIdResult = args.nextString();
-    if (tokenIdResult.isErr()) return new Result(0);
-    this.tokenId = tokenIdResult.unwrap();
-
-    const priceResult = args.nextU64();
-    if (priceResult.isErr()) return new Result(0);
-    this.price = priceResult.unwrap();
-
-    const buyerAddressResult = args.nextString();
-    if (buyerAddressResult.isErr()) return new Result(0);
-    this.buyerAddress = buyerAddressResult.unwrap();
-
-    const sellerAddressResult = args.nextString();
-    if (sellerAddressResult.isErr()) return new Result(0);
-    this.sellerAddress = sellerAddressResult.unwrap();
-
-    const timestampResult = args.nextString();
-    if (timestampResult.isErr()) return new Result(0);
-    this.timestamp = timestampResult.unwrap();
 
     return new Result(args.offset);
   }
