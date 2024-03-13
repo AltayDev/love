b15bab8bd06201c4cbc995809c81dff3eb008509
Oğuzhan <145108492+AltayDev@users.noreply.github.com>
2024-03-13 14:02:37 +0300

    Update NFT.ts

--- contracts/NFT.ts
+++ contracts/NFT.ts
@@ -1,235 +1,236 @@
-import * as internals from './NFT-internals';
+/**
+ *
+ * This is an example of an NFT contract that uses the NFT-internals
+ * helper functions to implement the ERC721 standard.
+ *
+ * This files does basically two things:
+ * 1. It wraps the NFT-internals functions, manages the deserialize/serialize of the arguments and return values,
+ *    and exposes them to the outside world.
+ * 2. It implements some custom features that are not part of the ERC721 standard, like mint, burn or ownership.
+ *
+ * The NFT-internals functions are not supposed to be re-exported by this file.
+ */
+
 import {
+  Args,
   boolToByte,
   stringToBytes,
   u256ToBytes,
-  u32ToBytes,
-  bytesToString,
-  bytesToU256,
-  byteToBool,
-  bytesToU64,
-  u64ToBytes,
 } from '@massalabs/as-types';
-import { Address, Context, Storage } from '@massalabs/massa-as-sdk';
-import { Args } from '@massalabs/as-types';
-import { u256 } from 'as-bignum/assembly';
+import {
+  _approve,
+  _balanceOf,
+  _constructor,
+  _getApproved,
+  _isApprovedForAll,
+  _name,
+  _ownerOf,
+  _setApprovalForAll,
+  _symbol,
+  _update,
+  _transferFrom,
+} from './NFT-internals';
+import { setOwner, onlyOwner } from '../utilities/ownership';
 
-export const OWNABLE_KEY = 'OWNABLE';
+import { Context, isDeployingContract, Storage } from '@massalabs/massa-as-sdk';
+
 export const BASE_URI_KEY = stringToBytes('BASE_URI');
-export const TOKEN_URI_KEY = stringToBytes('TOKEN_URI');
-export const TOTAL_SUPPLY_KEY = stringToBytes('TOTAL_SUPPLY');
-export const COUNTER_KEY = stringToBytes('COUNTER');
-export const MINT_PRICE_KEY = stringToBytes('PRICE_PER_TOKEN');
-export const START_TIME_KEY = stringToBytes('START_TIME');
-export const MINT_PAUSED_KEY = stringToBytes('MINT_PAUSED');
 
-// @custom:security-contact altailabs
-// init price = 0.036 MAS
-// mint price = 0.016 MAS
-// approve = 0.007 MAS
-
-export function constructor(_args: StaticArray<u8>): void {
-  assert(Context.isDeployingContract());
-  const args = new Args(_args);
-
+/**
+ * @param binaryArgs - serialized strings representing the name and the symbol of the NFT
+ *
+ * @remarks This is the constructor of the contract. It can only be called once, when the contract is being deployed.
+ * It expects two serialized arguments: the name and the symbol of the NFT.
+ * Once the constructor has handled the deserialization, of the arguments,
+ * it calls the _constructor function from the NFT-internals.
+ *
+ * Finally, it sets the owner of the contract to the caller of the constructor.
+ */
+export function constructor(binaryArgs: StaticArray<u8>): void {
+  assert(isDeployingContract());
+  const args = new Args(binaryArgs);
   const name = args.nextString().expect('name argument is missing or invalid');
   const symbol = args
     .nextString()
     .expect('symbol argument is missing or invalid');
-  const totalSupply = args
-    .nextU256()
-    .expect('totalSupply argument is missing or invalid');
   const baseURI = args
     .nextString()
     .expect('baseURI argument is missing or invalid');
-  const tokenURI = args
+  const ownerAddress = args
     .nextString()
-    .expect('tokenURI argument is missing or invalid');
-  const mintPrice = args
-    .nextU64()
-    .expect('mintPrice argument is missing or invalid');
-  const startTime = args
-    .nextU64()
-    .expect('startTime argument is missing or invalid');
+    .expect('ownerAddress argument is missing or invalid');
 
-  const constructorArgs = new Args().add(name).add(symbol).serialize();
-  internals._constructor(constructorArgs);
-
-  Storage.set(TOTAL_SUPPLY_KEY, u256ToBytes(totalSupply));
+  _constructor(name, symbol);
   Storage.set(BASE_URI_KEY, stringToBytes(baseURI));
-  Storage.set(TOKEN_URI_KEY, stringToBytes(tokenURI));
-  Storage.set(MINT_PRICE_KEY, u64ToBytes(mintPrice));
-  Storage.set(START_TIME_KEY, u64ToBytes(startTime));
-  Storage.set(MINT_PAUSED_KEY, boolToByte(false));
-  Storage.set(OWNABLE_KEY, Context.caller().toString());
-  Storage.set(COUNTER_KEY, u256ToBytes(u256.Zero));
+  setOwner(new Args().add(ownerAddress).serialize());
 }
 
-export function name(
-  _: StaticArray<u8> = new StaticArray<u8>(0),
-): StaticArray<u8> {
-  return Storage.get(internals.NAME_KEY);
+export function name(): string {
+  return _name();
 }
 
-export function symbol(
-  _: StaticArray<u8> = new StaticArray<u8>(0),
-): StaticArray<u8> {
-  return Storage.get(internals.SYMBOL_KEY);
+export function symbol(): string {
+  return _symbol();
 }
 
-export function totalSupply(
-  _: StaticArray<u8> = new StaticArray<u8>(0),
-): StaticArray<u8> {
-  return Storage.get(TOTAL_SUPPLY_KEY);
-}
-
-export function currentSupply(
-  _: StaticArray<u8> = new StaticArray<u8>(0),
-): StaticArray<u8> {
-  return Storage.get(COUNTER_KEY);
-}
-
-export function mint(_args: StaticArray<u8>): void {
-  assert(
-    bytesToU256(Storage.get(TOTAL_SUPPLY_KEY)) > bytesToU256(currentSupply()),
-    'Max supply reached',
-  );
-  assert(!byteToBool(Storage.get(MINT_PAUSED_KEY)), 'Mint process paused');
-  assert(
-    Context.timestamp() >= bytesToU64(Storage.get(START_TIME_KEY)),
-    'Mint has not started yet',
-  );
-  assert(
-    Context.transferredCoins() >= bytesToU64(Storage.get(MINT_PRICE_KEY)),
-    'Not enough sent coins to mint this NFT',
-  );
-  const args = new Args(_args);
-  const mintAddress = new Address(
-    args.nextString().expect('mintAddress argument is missing or invalid'),
-  );
-
-  const increment = bytesToU256(currentSupply()) + u256.One;
-  Storage.set(COUNTER_KEY, u256ToBytes(increment));
-  internals._update(mintAddress, increment, new Address());
-}
-
-export function tokenURI(_args: StaticArray<u8>): StaticArray<u8> {
-  const args = new Args(_args);
-  const tokenId = args
-    .nextU256()
-    .expect('token id argument is missing or invalid')
-    .toString();
-
-  const uri = bytesToString(Storage.get(TOKEN_URI_KEY));
-  const key = uri + tokenId;
-  return stringToBytes(key);
-}
-
-export function _setTokenURI(_args: StaticArray<u8>): void {
-  assert(_onlyOwner(), 'only sc owner can access');
-  const args = new Args(_args);
-  const newTokenURI = args
-    .nextString()
-    .expect('tokenUri argument is missing or invalid');
-
-  Storage.set(TOKEN_URI_KEY, stringToBytes(newTokenURI));
-}
-
-export function baseURI(
-  _: StaticArray<u8> = new StaticArray<u8>(0),
-): StaticArray<u8> {
-  return Storage.get(BASE_URI_KEY);
-}
-
-export function _setBaseURI(_args: StaticArray<u8>): void {
-  assert(_onlyOwner(), 'only sc owner can access');
-  const args = new Args(_args);
-  const newBaseURI = args
-    .nextString()
-    .expect('tokenUri argument is missing or invalid');
-
-  Storage.set(BASE_URI_KEY, stringToBytes(newBaseURI));
-}
-
-export function balanceOf(_args: StaticArray<u8>): StaticArray<u8> {
-  const args = new Args(_args);
+/**
+ *
+ * @param binaryArgs - serialized string representing the address whose balance we want to check
+ * @returns a serialized u256 representing the balance of the address
+ * @remarks As we can see, instead of checking the storage directly,
+ * we call the _balanceOf function from the NFT-internals.
+ */
+export function balanceOf(binaryArgs: StaticArray<u8>): StaticArray<u8> {
+  const args = new Args(binaryArgs);
   const address = args
     .nextString()
     .expect('address argument is missing or invalid');
-  return u256ToBytes(internals._balanceOf(new Address(address)));
+  return u256ToBytes(_balanceOf(address));
 }
 
-export function ownerOf(_args: StaticArray<u8>): StaticArray<u8> {
-  const args = new Args(_args);
+/**
+ *
+ * @param binaryArgs - serialized u256 representing the tokenId whose owner we want to check
+ * @returns a serialized string representing the address of owner of the tokenId
+ */
+export function ownerOf(binaryArgs: StaticArray<u8>): StaticArray<u8> {
+  const args = new Args(binaryArgs);
   const tokenId = args
     .nextU256()
-    .expect('token id argument is missing or invalid');
-
-  return stringToBytes(internals._ownerOf(tokenId).toString());
+    .expect('tokenId argument is missing or invalid');
+  return stringToBytes(_ownerOf(tokenId));
 }
 
-export function safeTransferFrom(_args: StaticArray<u8>): void {
-  const args = new Args(_args);
+/**
+ *
+ * @param binaryArgs - serialized u256 representing the tokenId whose approved address we want to check
+ * @returns a serialized string representing the address of the approved address of the tokenId
+ */
+export function getApproved(binaryArgs: StaticArray<u8>): StaticArray<u8> {
+  const args = new Args(binaryArgs);
+  const tokenId = args
+    .nextU256()
+    .expect('tokenId argument is missing or invalid');
+  return stringToBytes(_getApproved(tokenId));
+}
+
+/**
+ *
+ * @param binaryArgs - serialized strings representing the address of an owner and an operator
+ * @returns a serialized u8 representing a boolean value indicating if
+ * the operator is approved for all the owner's tokens
+ */
+export function isApprovedForAll(binaryArgs: StaticArray<u8>): StaticArray<u8> {
+  const args = new Args(binaryArgs);
+  const owner = args
+    .nextString()
+    .expect('owner argument is missing or invalid');
+  const operator = args
+    .nextString()
+    .expect('operator argument is missing or invalid');
+  return boolToByte(_isApprovedForAll(owner, operator));
+}
+
+/**
+ *
+ * @param binaryArgs - serialized strings representing the address of the recipient and the tokenId to approve
+ * @remarks This function is only callable by the owner of the tokenId or an approved operator.
+ * Indeed, this will be checked by the _approve function of the NFT-internals.
+ *
+ */
+export function approve(binaryArgs: StaticArray<u8>): void {
+  const args = new Args(binaryArgs);
+  const to = args.nextString().expect('to argument is missing or invalid');
+  const tokenId = args
+    .nextU256()
+    .expect('tokenId argument is missing or invalid');
+  _approve(to, tokenId);
+}
+
+/**
+ *
+ * @param binaryArgs - serialized arguments representing the address of the operator and a boolean value indicating
+ * if the operator should be approved for all the caller's tokens
+ *
+ */
+export function setApprovalForAll(binaryArgs: StaticArray<u8>): void {
+  const args = new Args(binaryArgs);
+  const to = args.nextString().expect('to argument is missing or invalid');
+  const approved = args
+    .nextBool()
+    .expect('approved argument is missing or invalid');
+  _setApprovalForAll(to, approved);
+}
+
+/**
+ *
+ * @param binaryArgs - serialized arguments representing the address of the sender,
+ * the address of the recipient and the tokenId to transfer.
+ *
+ * @remarks This function is only callable by the owner of the tokenId or an approved operator.
+ *
+ */
+export function transferFrom(binaryArgs: StaticArray<u8>): void {
+  const args = new Args(binaryArgs);
   const from = args.nextString().expect('from argument is missing or invalid');
   const to = args.nextString().expect('to argument is missing or invalid');
   const tokenId = args
     .nextU256()
-    .expect('token id argument is missing or invalid');
-
-  internals._safeTransferFrom(new Address(from), new Address(to), tokenId);
+    .expect('tokenId argument is missing or invalid');
+  _transferFrom(from, to, tokenId);
 }
 
-export function approve(_args: StaticArray<u8>): void {
-  const args = new Args(_args);
-  const toAddress = new Address(args.nextString().expect(''));
+/**
+ *
+ * @param binaryArgs - serialized arguments representing the address of the recipient and the tokenId to mint
+ *
+ * @remarks This function is only callable by the owner of the contract.
+ *
+ * This function is not part of the ERC721 standard.
+ * It serves as an example of how to use the NFT-internals functions to implement custom features.
+ * Here we make use of the _update function from the NFT-internals to mint a new token.
+ * Indeed, by calling _update with a non-existing tokenId, we are creating a new token.
+ *
+ * We also make sure that the mint feature is only callable by the owner of the contract
+ * by using the onlyOwner modifier.
+ *
+ */
+export function mint(binaryArgs: StaticArray<u8>): void {
+  onlyOwner();
+  const args = new Args(binaryArgs);
+  const to = args.nextString().expect('to argument is missing or invalid');
   const tokenId = args
     .nextU256()
-    .expect('token id argument is missing or invalid');
-
-  internals._approve(toAddress, tokenId);
+    .expect('tokenId argument is missing or invalid');
+  _update(to, tokenId, '');
 }
 
-export function setApprovalForAll(_args: StaticArray<u8>): void {
-  const args = new Args(_args);
-  const operatorAddress = new Address(
-    args.nextString().expect('operatorAddress argument is missing or invalid'),
-  );
-  const approved = args
-    .nextBool()
-    .expect('approved argument is missing or invalid');
-
-  internals._setApprovalForAll(operatorAddress, approved);
-}
-
-export function getApproved(_args: StaticArray<u8>): StaticArray<u8> {
-  const args = new Args(_args);
+/**
+ *
+ * @param binaryArgs - serialized u256 representing the tokenId to burn
+ *
+ * @remarks This function is not part of the ERC721 standard.
+ * It serves as an example of how to use the NFT-internals functions to implement custom features.
+ * Here we make use of the _update function from the NFT-internals to burn a token.
+ * Indeed, by calling _update with the zero address as a recipient, we are burning the token.
+ *
+ * We also made sure that the burn feature is only callable by the owner of the token or an approved operator.
+ * Indeed, the _update function will check if the caller is the owner of the token or an approved operator.
+ *
+ */
+export function burn(binaryArgs: StaticArray<u8>): void {
+  onlyOwner();
+  const args = new Args(binaryArgs);
   const tokenId = args
     .nextU256()
-    .expect('token id argument is missing or invalid');
-
-  return stringToBytes(internals._getApproved(tokenId).toString());
+    .expect('tokenId argument is missing or invalid');
+  _update('', tokenId, '');
 }
 
-export function isApprovedForAll(_args: StaticArray<u8>): StaticArray<u8> {
-  const args = new Args(_args);
-  const ownerAddress = new Address(
-    args.nextString().expect('ownerAddress argument is missing or invalid'),
-  );
-  const operatorAddress = new Address(
-    args.nextString().expect('operatorAddress argument is missing or invalid'),
-  );
-  return internals._isApprovedForAll(ownerAddress, operatorAddress)
-    ? u32ToBytes(1)
-    : u32ToBytes(0);
-}
-
-export function _onlyOwner(): bool {
-  return Context.caller().toString() == Storage.get(OWNABLE_KEY);
-}
-
-export function _changePauseStatus(_args: StaticArray<u8>): void {
-  assert(_onlyOwner(), 'only sc owner can access');
-  const args = new Args(_args);
-  const pause = args.nextBool().expect('pause argument is missing or invalid');
-  Storage.set(MINT_PAUSED_KEY, boolToByte(pause));
-}
+/**
+ * Here we re-export the ownerAddress function from the ownership file.
+ * This will allow the outside world to check the owner of the contract.
+ * However we do not re-export any function from the NFT-internals file.
+ * This is because the NFT-internals functions are not supposed to be called directly by the outside world.
+ */
+export { ownerAddress } from '../utilities/ownership';
