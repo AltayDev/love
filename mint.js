ef9538cb6fe27f70017fa856312b272b4d6a1b15
Oğuzhan <145108492+AltayDev@users.noreply.github.com>
2024-07-10 17:29:28 +0300

    Update mint.js

--- mint.js
+++ mint.js
@@ -1,41 +1,50 @@
-const {
-  ClientFactory,
-  WalletClient,
-  DefaultProviderUrls,
-  CHAIN_ID,
-  Args,
-  fromMAS,
-  MAX_GAS_CALL
-} = require("@massalabs/massa-web3");
+const {  
+  ClientFactory,  
+  WalletClient,  
+  DefaultProviderUrls,  
+  CHAIN_ID,  
+  Args,  
+  fromMAS,  
+  MAX_GAS_CALL  
+} = require("@massalabs/massa-web3");  
+  
+const secretKey = "S1zN1YEnNeEws7nfULuCmZsi";  
+const buildnetRpc = "https://buildnet.massa.net/api/v2";  
+  
+async function initializeClient() {  
+  const deployedAccount = await WalletClient.getAccountFromSecretKey(secretKey);  
+  
+  const client = await ClientFactory.createDefaultClient(  
+    buildnetRpc,  
+    CHAIN_ID.BuildNet,  
+    false,  
+    deployedAccount  
+  );  
+  
+  return client;  
+}  
+  
+async function mint(wallet) {  
+  try {
+    if (!wallet) {
+      throw new Error("Wallet address is undefined or null");
+    }
 
-const secretKey = "YOUR SECRET KEY ( CONTRACT OWNER )";
-const mainnetRPC = "https://mainnet.massa.net/api/v2";
-
-async function initializeClient() {
-  const deployedAccount = await WalletClient.getAccountFromSecretKey(secretKey);
-
-  const client = await ClientFactory.createDefaultClient(
-    mainnetRPC,
-    CHAIN_ID.MainNet,
-    false,
-    deployedAccount
-  );
-
-  return client;
-}
-
-async function mint(wallet) {
-  const client = await initializeClient()
-  const tx = await client.smartContracts().callSmartContract({
-    fee: fromMAS(0.01),
-    maxGas: MAX_GAS_CALL,
-    coins: fromMAS(0),
-    targetAddress: 'SMART_CONTRACT_ADDRESS',
-    functionName: 'mint',
-    parameter: new Args().addString(wallet),
-  });
-  console.log(tx);
-}
-
-const walletAddress = process.argv[2];
+    const client = await initializeClient();
+    const tx = await client.smartContracts().callSmartContract({  
+      fee: fromMAS(0.01),  
+      maxGas: MAX_GAS_CALL,  
+      coins: fromMAS(0),  
+      targetAddress: 'AS1JV2i9pHHZGwoUWE8JvMGB7F2K3nL1iKFgjRTLAtwTdDsLXM7u',  
+      functionName: 'mint',  
+      parameter: new Args().addString(wallet),  
+    });  
+    console.log(tx);
+  } catch (error) {
+    console.error("Error in mint function:", error);
+  }
+}  
+  
+const walletAddress = process.argv[2];  
+console.log("Wallet address received:", walletAddress);  // Debugging line
 mint(walletAddress);
